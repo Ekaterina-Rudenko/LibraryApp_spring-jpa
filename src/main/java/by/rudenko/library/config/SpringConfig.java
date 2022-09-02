b725ddb748846c9a9e7bfc90a0c1460cb1982370
Ekaterina-Rudenko <katestasevich30@gmail.com>
2022-09-02 03:27:04 +0300

    fixing

--- src/main/java/by/rudenko/library/config/SpringConfig.java
+++ src/main/java/by/rudenko/library/config/SpringConfig.java
@@ -1,6 +1,5 @@
 package by.rudenko.library.config;
 
-import java.util.Objects;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/by/rudenko/library/repositories/BooksRepository.java
+++ src/main/java/by/rudenko/library/repositories/BooksRepository.java
@@ -3,13 +3,10 @@ package by.rudenko.library.repositories;
 import by.rudenko.library.models.Book;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.expression.spel.support.ReflectivePropertyAccessor.OptimalPropertyAccessor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BooksRepository extends JpaRepository<Book, Integer> {
-
-  List<Book> findByOwnerId(int id);
 
   List<Book> findBookByTitleAndAuthor(String title, String author);
 
